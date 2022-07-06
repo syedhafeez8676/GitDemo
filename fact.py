427ff6f247440e0ea0eef92904a606d4ac12d930
Syed Niaz <syedhafeez8676@gmail.com>
2022-07-06 23:36:20 +0530

    added iterative factorial to fact.py

--- fact.py
+++ fact.py
@@ -3,3 +3,9 @@ def recursiveFact(a):
         return 1
     else:
         return a*recursiveFact(a-1)
+
+def iterativeFact(a):
+    res=1
+    for i in range(2,a+1):
+        res*=i
+    return res
